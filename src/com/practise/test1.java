ea22d2a25e8fd3dbe17adb4938b8cbc521545bc6
saikrishna <saikrishnaakepati@gmail.com>
2023-11-11 17:23:00 +0530

    modified again

--- src/com/practise/test1.java
+++ src/com/practise/test1.java
@@ -3,7 +3,7 @@ package com.practise;
 public class test1 {
 
 	public static void main(String[] args) {
-		String input = "AAAAABBCCDDA";
+		String input = "AAAAABBCCDD";
 		String output = countChar(input);
 
 		String output2 = countCharAll(input);
