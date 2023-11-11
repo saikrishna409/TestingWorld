4b842c73aeab3a60900ccf17478d9b7106d901b7
saikrishna <saikrishnaakepati@gmail.com>
2023-11-11 16:18:55 +0530

    Modified String input

--- src/com/practise/test1.java
+++ src/com/practise/test1.java
@@ -3,7 +3,7 @@ package com.practise;
 public class test1 {
 
 	public static void main(String[] args) {
-		String input = "AAAAABBCCAAA";
+		String input = "AAAAABBCCAA";
 		String output = countChar(input);
 
 		String output2 = countCharAll(input);
